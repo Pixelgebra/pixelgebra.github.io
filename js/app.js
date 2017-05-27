6e6864c0e05d6a75b90789b6b554866afec62591
ChiaChi Tsai <rueshyna@gmail.com>
2017-05-27 15:33:47 +0800

    add checking for param

--- js/app.js
+++ js/app.js
@@ -2,14 +2,32 @@
 $(document).foundation();
 
 <!-- JS := GoogleAnalyticsObject -->
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)
-},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
 
-ga('create', 'UA-99849613-1', 'auto');
-ga('send', 'pageview');
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+var st = getUrlParameter('st');
+if ( st === undefined || st !== '1') {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)
+  },i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-99849613-1', 'auto');
+  ga('send', 'pageview');
+};
 
 <!-- JS := Disqus-->
 (function() { // DON'T EDIT BELOW THIS LINE
